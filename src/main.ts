9c21e34912e24ef9f96b3d6cb01fa9f9bd9e9418
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2026-06-05 13:28:38 +0700

    feat: update domain

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const PORT = process.env.PORT || 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://news-portal-psi-wine.vercel.app',
-    ],
+    origin: ['http://localhost:3000', 'https://www.baoxuan-demo.io.vn'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 5000);
-  console.log(`🚀 Backend running on http://localhost:5000/api/v1`);
+  await app.listen(PORT);
+
+  console.log(`🚀 Backend running on ${PORT}`);
 }
+
 bootstrap();
